4a3381fa3b5550382f247af49c191881b7020276
rishika <rishikareddy.rishii@gmail.com>
2019-03-12 03:04:03 +0530

    mavenproject added

---
+++ mavenpro1/src/main/java/MavenClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rishika
+ */
+public class MavenClass {
+ public static void main(String[] st){
+     System.out.println("Hello");
+ }  
+}
